e5ad9b01302bc2ad14e8fec93fc022ef68e90564
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-04-08 22:53:16 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -128,27 +128,27 @@ def game_hash
   }
 end
 
-def num_points_scored(player_name)
-  game_hash.each do |place_key, item_hash|
-    item_hash[:players].each do |item|
-      if item[:player_name] == player_name
-            # binding.pry
-        return item[:points]
-      end
-    end
-  end
-end
-
-def shoe_size(player_name)
-  game_hash.each do |place_key, item_hash|
-                # binding.pry
-    item_hash[:players].each do |item|
-      if item[:player_name] == player_name
-        return item[:shoe]
-      end
-    end
-  end
-end
+# def num_points_scored(player_name)
+#   game_hash.each do |place_key, item_hash|
+#     item_hash[:players].each do |item|
+#       if item[:player_name] == player_name
+#             # binding.pry
+#         return item[:points]
+#       end
+#     end
+#   end
+# end
+#
+# def shoe_size(player_name)
+#   game_hash.each do |place_key, item_hash|
+#                 # binding.pry
+#     item_hash[:players].each do |item|
+#       if item[:player_name] == player_name
+#         return item[:shoe]
+#       end
+#     end
+#   end
+# end
 
 def team_colors(team_name)
   game_hash.each do |place_key, item_hash|
