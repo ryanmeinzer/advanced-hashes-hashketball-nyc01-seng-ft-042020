77238f2782010e27681cc0c99daba3fde17b3a7d
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-04-08 22:34:16 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -138,3 +138,14 @@ def num_points_scored(player_name)
     end
   end
 end
+
+def shoe_size(player_name)
+  game_hash.each do |place_key, item_hash|
+    item_hash[:players].each do |item|
+      if item[:player_name] == player_name
+            # binding.pry
+        return item[:shoe]
+      end
+    end
+  end
+end
