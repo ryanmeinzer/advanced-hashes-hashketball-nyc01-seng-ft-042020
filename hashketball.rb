a946add2d18f6c228e12ae85938979b6fd1b7283
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-04-09 00:15:56 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -168,7 +168,7 @@ def player_numbers(team_name)
   game_hash.each do |place_key, item_hash|
             binding.pry
     if item_hash[:team_name] == team_name
-      item_hash.collect do |item|
+      item_hash[:players].collect do |item|
         # item[:players][:number]
       end
     end
